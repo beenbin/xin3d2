8c6f4f5cb2cc24091138e602a2695e8a31a4f7b1
Bingo <61998928+beenbin@users.noreply.github.com>
2021-10-20 19:59:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/beenbin/Lienol_Passwall.git package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone -b master https://github.com/vernesong/OpenClash.git package/Openclash
+#git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+#git clone https://github.com/tano-systems/luci-theme-tano.git package/lean/luci-theme-tano
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
