6a7d6d5ad0d07217c96357ac53fad530e1cde30c
Bingo <61998928+beenbin@users.noreply.github.com>
2022-02-11 18:30:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,21 @@
 sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/beenbin/Lienol_Passwall.git package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+#git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
+
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/kenzok8/small-package/luci-app-adguardhome.git package/luci-app-adguardhome
+
 git clone -b master https://github.com/vernesong/OpenClash.git package/Openclash
+git clone -b master https://github.com/kenzok8/luci-app-store.git package/luci-app-store
+
 #git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+git clone -b master https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
 git clone https://github.com/tano-systems/luci-theme-tano.git package/lean/luci-theme-tano
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/kenzok8/small-package/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/kenzok8/small-package/luci-theme-edge.git package/lean/luci-theme-edge
+git clone https://github.com/kenzok8/small-package/luci-theme-mcat.git package/lean/luci-theme-mcat
+git clone https://github.com/kenzok8/small-package/luci-theme-argonne.git package/lean/luci-theme-argonne
+git clone https://github.com/kenzok8/small-package/luci-theme-atmaterial_new.git package/lean/luci-theme-atmaterial_new
